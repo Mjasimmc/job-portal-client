aa06385ff025e0bc85540c3be246c0d95910b055
Mjasim <mjasimmc@gmail.com>
2024-02-01 09:51:35 +0530

    completed working

--- src/config_Api.jsx
+++ src/config_Api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const renderServer = 'https://server-careerharbor.onrender.com'
 // const awsServer = 'http://13.60.23.95'
-// const localServer = 'http://localhost:4000'
+const localServer = 'http://localhost:4000'
 export const BaseURL = renderServer
 
 const ApiCall = axios.create({
